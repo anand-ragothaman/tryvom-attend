a3c19ac1201d365051f747ef8193b73dc58ef7ec
MoviesLiker <anandrago.18r@gmail.com>
2024-08-25 16:53:23 +0530

    menu added

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,12 @@
+
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Auth.login, name='auth.login'),
+    path('', views.login_view, name='auth.login'),
+    path('auth/logout/', views.logout_view, name='auth.logout'),
 
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('users/all-users', views.dashboard, name='users.all_users'),
+    path('users/all-users', views.dashboard, name='users.add_user'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,26 +1,80 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib import messages
+from app.decorators import unauthenticated_user
 from app.forms import LoginForm
 
 
-class Auth:
-    def login(request):
-        if request.method == 'POST':
-            form = LoginForm(request.POST)
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password')
-                user = authenticate(request, username=username, password=password)
-                if user is not None:
-                    login(request, user)
-                    return redirect('dashboard')  # Redirect to a success page.
-                else:
-                    form.add_error(None, "Invalid username or password")
-        else:
-            form = LoginForm()
-        return render(request, 'auth/login.html', {'form': form})
+@unauthenticated_user
+def login_view(request):
+    if 'next' in request.GET:
+        next_route = request.GET['next']
+    else:
+        next_route = None
 
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                if next_route != None:
+                    return redirect(next_route)
+                else:
+                    return redirect('dashboard')  # Redirect to a success page.
+            else:
+                form.add_error(None, "Invalid username or password")
+    else:
+        form = LoginForm()
+
+
+
+    data = {
+        'form': form,
+        'next_route': next_route
+    }
+
+    return render(request, 'auth/login.html', data)
+
+def logout_view(request):
+    logout(request)
+    messages.add_message(request, messages.SUCCESS, "Logged out successfully!")
+    return redirect('/')
+
+@login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    menus = [
+            {
+                'name': 'Dashboard',
+                'icon':'d',
+                'route':'dashboard',
+                'type': 0
+            },
+         {
+              'name': 'Users',
+              'icon':'u',
+              'type': 1,
+              'sub_menus':[
+                {
+                    'name': 'All Users',
+                    'route':'users.all_users',
+                },{
+                    'name': 'Add User',
+                    'route':'users.add_user',
+                }
+            ]
+                
+         } 
+            
+    ]
+    
+    
+    
+    data={
+        'menus':menus
+    }
+    return render(request, 'dashboard.html', data)
