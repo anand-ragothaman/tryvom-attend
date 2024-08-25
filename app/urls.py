b35db70d1e48d4f7c036054d4ea11f70aa478bd7
MoviesLiker <anandrago.18r@gmail.com>
2024-08-25 13:20:25 +0530

    enter dashboard

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
-
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.Auth.login, name='auth.login'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from app.forms import LoginForm
+
 
 class Auth:
     def login(request):
-        return render(request, 'auth/login.html')
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('dashboard')  # Redirect to a success page.
+                else:
+                    form.add_error(None, "Invalid username or password")
+        else:
+            form = LoginForm()
+        return render(request, 'auth/login.html', {'form': form})
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
